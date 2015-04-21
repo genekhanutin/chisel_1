47ac98fd5a9df0ce07a5dde03ddb015bbf0e3fc5
genekhanutin <genekhanutin@gmail.com>
2015-04-21 09:57:32 -0600

    converts headers

---
+++ test/html_converter_test.rb
@@ -0,0 +1,43 @@
+require 'minitest/autorun'
+require './lib/html_converter'
+
+class HtmlConverterTest < Minitest::Test
+  def test_it_converts_to_h1_tags
+    html = HtmlConverter.new('# My Life in Desserts')
+    assert_equal '<h1>My Life in Desserts</h1>\n', html.convert_h1
+  end  
+
+  def test_it_converts_to_h2_tags
+    html = HtmlConverter.new('## My Life in Desserts')
+    assert_equal '<h2>My Life in Desserts</h2>\n', html.convert_h2
+  end
+
+  def test_it_converts_to_h3_tags
+    html = HtmlConverter.new('### My Life in Desserts')
+    assert_equal '<h3>My Life in Desserts</h3>\n', html.convert_h3
+  end
+
+  def test_it_converts_to_h4_tags
+    html = HtmlConverter.new('#### My Life in Desserts')
+    assert_equal '<h4>My Life in Desserts</h4>\n', html.convert_h4
+  end
+
+  def test_it_converts_to_h5_tags
+    html = HtmlConverter.new('##### My Life in Desserts')
+    assert_equal '<h5>My Life in Desserts</h5>\n', html.convert_h5
+  end  
+
+  def test_it_converts_any_header
+    h1 = HtmlConverter.new('# My Life in Desserts')
+    h2 = HtmlConverter.new('## My Life in Desserts')
+    h3 = HtmlConverter.new('### My Life in Desserts')
+    h4 = HtmlConverter.new('#### My Life in Desserts')
+    h5 = HtmlConverter.new('##### My Life in Desserts')
+
+    assert_equal '<h1>My Life in Desserts</h1>\n', h1.convert_headers
+    assert_equal '<h2>My Life in Desserts</h2>\n', h2.convert_headers
+    assert_equal '<h3>My Life in Desserts</h3>\n', h3.convert_headers
+    assert_equal '<h4>My Life in Desserts</h4>\n', h4.convert_headers
+    assert_equal '<h5>My Life in Desserts</h5>\n', h5.convert_headers
+  end  
+end
