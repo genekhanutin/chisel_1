cf5fec25d3e91ae7b7e7a0866f844b49ad562cef
genekhanutin <genekhanutin@gmail.com>
2015-04-22 16:20:39 -0600

    safety commit i

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -1,11 +1,12 @@
-require './lib/header_converter'     # ~> LoadError: cannot load such file -- ./lib/header_converter
+require './lib/header_converter'
 require './lib/paragraph_converter'
 require './lib/symbol_converter'
 
 class Chisel
   attr_accessor :message
+
   def initialize(message)
-    @message = File.readlines(ARGV[0])
+    @message = File.read(ARGV[0]).split('\n')
   end
 
   def convert_symbols
@@ -27,58 +28,3 @@ end
 
 chisel = Chisel.new(ARGV[0])
 puts chisel.convert_all_headers 
-    # require 'pry' ; binding.pry
-
-# class Chisel < HeaderConverter
-#   def initialize(text)
-#     @text             = File.readlines(ARGV[0])
-#     puts HeaderConverter.new(@text).convert_headers
-#   end
-
-# puts Chisel.new(@header_converter)
-
-# splat, double splat, & need to called on text all the time
-# cannot have branches inside of if statement
-
-  # def convert_to_html
-  #   message.map do |line|
-  #     if line.match(/^#/)
-  #       HeaderConverter.new(line).convert_headers
-  #     elsif line.match(/[*][*]/)
-  #       SymbolConverter.new(line).convert_strong_tags
-  #     elsif line.match(/[*]/)
-  #       SymbolConverter.new(line).convert_em
-  #     elsif line.match(/[&]/)
-  #       SymbolConverter.new(line).convert_ampersand_symbols
-  #     elsif line == "\n"
-  #       ''
-  #     else 
-  #       !line.match(/^# *(.*?)$/)
-  #       ParagraphCoverter.new(line).convert_p_tags
-  #     #   ParagraphCoverter.new(line).open_p_tags
-  #     # else !line.match('#')
-  #     #    ParagraphCoverter.new(line).close_p_tags
-  #     end
-  #   end
-  # end
-#   def convert
-#     @text.map do |line|
-#       if line.start_with?('#')
-#         HeaderConverter.new(line).convert_headers
-#       elsif line.start_with?('*')
-#         SymbolConverter.new(line).convert_nested_tags
-#       elsif line.include?('&') 
-#         SymbolConverter.new(line).convert_ampersand_symbols
-#       else
-#         ParagraphConverter.new.open_p_tags
-#       end
-#     end
-#   end
-# end
-
-# ~> LoadError
-# ~> cannot load such file -- ./lib/header_converter
-# ~>
-# ~> /Users/genekhanutin/.rvm/rubies/ruby-2.1.5/lib/ruby/site_ruby/2.1.0/rubygems/core_ext/kernel_require.rb:54:in `require'
-# ~> /Users/genekhanutin/.rvm/rubies/ruby-2.1.5/lib/ruby/site_ruby/2.1.0/rubygems/core_ext/kernel_require.rb:54:in `require'
-# ~> /Users/genekhanutin/module_1/home_work/chisel_1/lib/chisel.rb:1:in `<main>'
---
+++ test/list_converter_test.rb
@@ -0,0 +1,7 @@
+require 'minitest/autorun'
+require 'list_converter'
+
+class ListConverterTest < Minitest::Test
+  def test_it_adds_p
+end
+
