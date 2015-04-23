6658241a28247d5c2ea7b17de6ca3751521dfa78
genekhanutin <genekhanutin@gmail.com>
2015-04-23 13:23:26 -0600

    safety commit-too many ol tags not enough ul

--- test/paragraph_converter_test.rb
+++ test/paragraph_converter_test.rb
@@ -1,21 +1,21 @@
 require 'minitest/autorun'
 require './lib/paragraph_converter'
 
-class HtmlConverterTest < Minitest::Test
+class ParagraphConverterTest < Minitest::Test
   def test_it_adds_opening_p_tag
     text   = "Here is some paragraph text"
-    html   = ParagraphCoverter.new(text)
-    output =  "<p>\nHere is some paragraph text"
+    html   = ParagraphConverter.new(text)
+    output =  "<p>\nHere is some paragraph text\n</p>"
 
-    assert output, html.open_p_tags  
+    assert output, html.p_tags  
   end 
 
   def test_it_adds_closing_p_tags
    text   = 'Here is some paragraph text'
-   html   = ParagraphCoverter.new(text) 
-   output = 'Here is some paragraph text</p>'
+   html   = ParagraphConverter.new(text) 
+   output = "<p>\nHere is some paragraph text\n</p>"
 
-   assert output, html.close_p_tags
+   assert output, html.p_tags
   end
 end
 
