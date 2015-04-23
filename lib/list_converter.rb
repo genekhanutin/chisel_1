cc79211205d729450433f80a161575b5b701a777
genekhanutin <genekhanutin@gmail.com>
2015-04-23 14:54:20 -0600

    list converter test pass

--- lib/list_converter.rb
+++ lib/list_converter.rb
@@ -6,7 +6,6 @@ class ListConverter
   end
 
   def convert_ul_items
-  # require 'pry' ; binding.pry 
     if text.start_with?("*")
       "<ul>\n" + text.gsub!(/\* (.+)/,"<li>\\1</li>") + "\n</ul>\n"
     else
@@ -15,22 +14,10 @@ class ListConverter
   end
 
   def convert_ol_items
-    if text[0].to_i > 0
-      text.gsub(/^\d+\. (.+)/,"<li>\\1</li>")
+    if text[0].to_i > 0 
+      "<ol>\n" + text.gsub(/^\d+\. (.+)/,"<li>\\1</li>") + "</ol>"
     else
       text
     end
   end
 end
-  # def convert_ul_items
-  #   if text.start_with?("* ")
-  #     text.gsub!(/[*] (.*)$/, "<li>\\1</li>")
-  #   end
-  # end
-
-  # def convert_ol_items
-  #   tag_count = 0
-  #   if text.scan(/^[0-9]./)
-  #     text.gsub!(/^[0-9]. (.*)$/, "<li>\\1</li>")
-  #   end
-  # end  
